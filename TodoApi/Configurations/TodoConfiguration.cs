a59169af48945e2fc28b512eaa9e7e40c97aec1c
IdoAmar <idoamar2412@gmail.com>
2021-06-07 16:30:19 +0300

    added another unadded project

---
+++ TodoApi/Configurations/TodoConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TodoApi.Models;
+
+namespace TodoApi.Configurations
+{
+    public class TodoConfiguration : IEntityTypeConfiguration<TodoItem>
+    {
+        public void Configure(EntityTypeBuilder<TodoItem> builder)
+        {
+            builder.ToTable("Todos");
+
+            builder.HasKey(t => t.Id);
+
+            builder.Property(t => t.IsComplete)
+                   .IsRequired();
+
+            builder.Property(t => t.Name)
+                   .IsRequired();
+
+            builder.Property(t => t.Secret);
+        }
+    }
+}
---
+++ TodoApi/Models/TodoDBContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TodoApi.Configurations;
+
+namespace TodoApi.Models
+{
+    public class TodoDBContext : DbContext
+    {
+        public TodoDBContext(DbContextOptions<TodoDBContext> options)
+            : base(options)
+            {
+            }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(TodoItem).Assembly);
+            //modelBuilder.ApplyConfiguration<TodoItem>(new TodoConfiguration());
+        }
+    }
+}
